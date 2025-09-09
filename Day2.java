98b8bad689f22584f70d2963ec1fb48782a9923f
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-09 12:04:53 +0800

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -8,7 +8,7 @@ public class RizaldiD0225009 {
         System.out.print("Prodi Informatika");
         System.out.print("Umur saya 17 tahun");
         System.out.println();
-        System.out.printf("\nNama saya %s %nUmur %d %nProdi Informatika  ","Rizaldi",17,"Informatika");
+        System.out.printf("\nNama saya %s %nUmur %d %nProdi %s ","Rizaldi",17,"Informatika");
         
     }
     

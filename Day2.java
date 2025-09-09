cae44c5737c9ca4fcf9de19fd4996c7d53755120
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-09 11:58:48 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,15 @@
+public class RizaldiD0225009 {  
+    public static void main(String[] args) {
+        System.out.println("Nama saya Rizaldi");
+        System.out.println("Prodi Informatika ");
+        System.out.println("Umur saya 17 tahun ");
+        System.out.println();
+        System.out.print("Nama saya Rizaldi");
+        System.out.print("Prodi Informatika");
+        System.out.print("Umur saya 17 tahun");
+        System.out.println();
+        System.out.printf("\nNama saya %s %nUmur %d %nProdi Informatika  ","Rizaldi",17,"Informatika");
+        
+    }
+    
+}

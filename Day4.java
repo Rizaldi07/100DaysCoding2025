43f9fa570e79113e5b28db88c9b18f170a78f7d6
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-11 00:06:24 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,20 @@
+public class RizaldiD0225009 {  
+    public static void main(String[] args) {
+        //Memahami dan tau cara penggunaan komentar.
+        //Println di gunakan untuk mencetak teks ke layar untuk setiap baris
+        System.out.println("Nama saya Rizaldi");
+        System.out.println("Prodi Informatika ");
+        System.out.println("Umur saya 18 tahun ");
+        System.out.println();
+        //Print diguankan untuk mencetak teks ke layar tanpa membuat baris baru
+        // atau tanpa berpindah ke baris baru
+        System.out.print("Nama saya Rizaldi");
+        System.out.print(", Prodi Informatika");
+        System.out.print(", Umur saya 18 tahun.");
+        System.out.println();
+        //Printf digunakan untuk mencetak teks dengan format tertentu, contohnya yaitu format (%s) untuk String dan (%d) untuk Integer
+        System.out.printf("\nNama saya %s %nUmur %d %nProdi Informatika  ","Rizaldi",18,"Informatika");
+        
+    }
+    
+}

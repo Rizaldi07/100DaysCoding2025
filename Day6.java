3fa1b3f3f4e6b0ecf166d5b49350998d9514d96b
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-13 22:11:48 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,24 @@
+public class SaldiJhy {
+    public static void main(String[] args) {
+        //Contoh penggunaan tipe data float dan double
+        //float dan double sama-sama digunakan untuk menyimpan bilangan desimal
+        //Kedua tipe data ini digunakan untuk angka atau bilangan dengan koma bukan untuk bilangan bulat
+        //Contoh mencari luas dan keliling lingkaran
+        float phi = (float)3.14;
+        double jarijari,luas,keliling;
+        jarijari = 30;
+        luas = phi * jarijari;
+        keliling = 2 * phi * jarijari;
+        
+        //Perbedaan kedua tipe data ini yaitu float lebih ringan  sedangkan float lebih berat.
+        //tetapi float hasilnya kurang akurat sedangkan double lebih akurat dibanding dengan double
+        //Presisi untuk float sekitar 6 sampai 7 digit desimal sedangkan double sekitar 15 ampai 16 digit desimal
+        System.out.println("phi = "+phi);
+        System.out.println("jadi luas lingkaran adalah "+luas);
+        System.out.println("dan untuk keliling linkaran adalah "+keliling);
+       
+       
+       
+        
+    }
+}

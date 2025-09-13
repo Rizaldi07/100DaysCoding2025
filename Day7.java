b53fee08e1fecbf8504dcb4949e8ed80c0a21a79
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-14 02:48:03 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,34 @@
+public class SaldiJhy {
+    public static void main(String[] args) {
+        //Penggunaan tipe data char dan boolean
+        //Yang Pertama yaitu char
+        //Tipe data ini hanya menyimpan satu karakter unicode, bisa berupa angka,huruf dan simbol.
+        //sedikit mirip tipe data string,tipe data char menyimpan nilai dengan tanda kutip 1 ('').
+        char kelas,nomor,anakke,adik;
+        kelas = 'A';
+        nomor = '7';
+        anakke = '1';
+        adik = '2';
+        
+        // Yang kedua yaitu boolean
+        //tipe data boolean hanya bisa bernilai true dan false
+        // nilai dari variable tipe data boolean hanya bisa dua yaitu true dan false
+        boolean laki_laki,anak_ke2,mahasiswa,sisfo,informatika;
+        laki_laki = true;
+        anak_ke2 = false;
+        mahasiswa = true;
+        sisfo = false;
+        informatika = true;
+        boolean login = true;
+        boolean nlogin = false;
+        System.out.println("Saya Dari kelas\t \t : "+kelas);
+        System.out.println("nomor favorit saya nomor : "+nomor);
+        System.out.println("Saya adalah anak ke \t : "+anakke+"(pertama)");
+        System.out.println("Adik saya ada \t\t : "+adik);
+        System.out.println("\nApakah Jenis Kelamin anda laki-laki?\t :"+laki_laki);
+        System.out.println("Apakah anda anak kedua?\t\t\t :"+anak_ke2);
+        System.out.println("Apakah anda dari prodi Sisfo\t\t :"+sisfo);
+        System.out.println("Apakah anda dari prodi informatika\t :"+informatika);
+        
+    }
+}

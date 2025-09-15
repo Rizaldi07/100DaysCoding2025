d1559b32d4b2795b7904f725c962e952dd070122
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-15 18:16:32 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,29 @@
+public class SaldiJhy {
+    public static void main(String[] args) {
+       //Tipe data String, yaitu tipe data yang nilainya bebas selama ada didalam tanda kutip("")
+       // Tipe data string adalah tipe data non-primitif
+       String nama,umur,prodi,kelas,fakultas,No_Hp,tinggi,GOLDAR;
+       nama = "Rizaldi";
+       prodi = "Informatika";
+       fakultas = "Teknik";
+       umur = "18 Tahun";
+       kelas = "A";
+       No_Hp = "081222299045";
+       tinggi = "161";
+       GOLDAR = "O";
+       String ekspresi1,ekspresi2;
+       ekspresi1 = "(^_^) ";
+       ekspresi2 = "(-_-)";
+       //Seperti yang di lihat di atas,selama nilainya di dalam tanda kutip tidak akan error.
+        System.out.println(ekspresi1);
+        System.out.println("Nama\t: "+nama);
+        System.out.println("Prodi\t :"+prodi);
+        System.out.println("Nama\t: "+fakultas);
+        System.out.println("Nama\t: "+umur);
+        System.out.println("Nama\t: "+kelas);
+        System.out.println("Nama\t: "+No_Hp);
+        System.out.println("Nama\t: "+tinggi+" cm");
+        System.out.println("Goldar\t: "+GOLDAR);
+        System.out.println(ekspresi2);
+    }
+}

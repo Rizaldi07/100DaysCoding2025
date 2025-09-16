2b3aaf7b83d680461d2d0e2985a8c92b86364f85
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-16 19:35:12 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,28 @@
+public class SaldiJhy {
+    public static void main(String[] args) {
+        //Deklarasi itu pemberian atau pengisisian pada tipe data dan variable, contoh tipe data String untuk tipe data dan nama,umur,dan kelas itu variabelnya:
+        // jadi pada saat menginput tipe data dan varibel itu namanya deklarasi
+       String nama,umur,kelas,peringkat;
+       //Inisialisasi itu pemberian nilai untuk tipe data dan variabelnya,seperti yang ada di bawah, nilainya adalah Rizaldi,18 tahun, A, dan informatika:
+       nama = "Rizaldi";
+       umur = "18 tahun";
+       kelas = "A";
+       peringkat = "Pertama";
+       //Update itu kita mengganti nilai dari variabel yang kita inisialisasi tadi.
+       //Dikarenakan kita update, yang di ubah hanya nilainya,untuk tipe data dan varibel tidak diubah.
+       // Misalnya :
+       nama = "M.Jefri";
+       umur = "20 tahun";
+       kelas = "B";
+       peringkat = "Kedua";
+       // Jadi intinya adalah
+       //Deklarasi = Bikin wadah/membuat wadah untuk nilai
+       //Inisialisasi = mengisi wadah dengan nilai
+       //Update = ganti nilai dengan wadah yang sama.
+        System.out.println("Nama\t\t: "+nama);
+        System.out.println("Umur\t\t: "+umur);
+        System.out.println("Kelas\t\t: "+kelas);
+        System.out.println("Peringkat\t: "+peringkat);
+       
+    }
+}

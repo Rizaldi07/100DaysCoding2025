4df6df5815bbe9d0c6a9f0b2ac9e5669732497e1
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-17 18:56:33 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,25 @@
+public class SaldiJhy {
+    public static void main(String[] args) {
+      final String Nama = "RIZALDI";
+      final int umur = 18;
+      final double tinggi = 161.5;
+      final boolean Laki = true;
+      final long No_Hp = 6285696223135l;
+      final byte No_Sepatu = 41;
+      final float bb = (float)59.5;
+      final short Saudara = 2;
+      final char kelas = 'A';
+      // final ini keyword atau penginputan yang tidak bisa di ubah lagi atau di update.jadi benar-benar inputan final.Ia akan besifat tetap.
+      // Variabelnya hanya bisa di isi hanya satu kali jika ada keyword atau kata kunci final.
+      //Setelah variabelnya diberi nilai maka tidak akan bisa diubah lagi
+        System.out.println("Nama\t\t: "+Nama);
+        System.out.println("umur\t\t: "+umur);
+        System.out.println("kelas\t\t: "+kelas);
+        System.out.println("Tinggi\t\t: "+tinggi);
+        System.out.println("BB\t\t: "+bb);
+        System.out.println("N0 Hp\t\t: "+No_Hp);
+        System.out.println("No Sepatu\t: "+No_Sepatu);
+        System.out.println("Laki?\t\t: "+Laki);
+        System.out.println("Saudara\t\t: "+Saudara);
+    }
+}

b8745a80537e5cfe39aa252db185633fc5c44ece
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-25 20:36:09 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,12 @@
+package D0225009;
+import java.util.Scanner;
+public class SaldiJhy {
+    public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+           System.out.print("Masukkan angka : ");
+           byte angka = in.nextByte();
+           short angka1 = angka;
+           System.out.println(""+angka1);
+      
+    }
+}

92864599ac127a54601d9a41fab3b8da7374d134
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-26 22:15:28 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,17 @@
+ 
+package D0225009;
+
+import java.util.Scanner;
+
+public class SaldiJhy {
+    public static void main(String[] args) {Scanner in = new Scanner(System.in);
+        System.out.print("masukkan angka : ");
+           long a = in.nextLong();
+           int b = (int)a;
+           System.out.print("Masukkan angka Kedua : ");
+           long c = in.nextLong();
+           byte d = (byte)c;
+           System.out.println(b);
+          System.out.println(d);
+  }
+}

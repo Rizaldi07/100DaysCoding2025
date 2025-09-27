ad6b2f412dd5cb20435f9e543c4b961762178cbb
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-27 18:04:36 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,12 @@
+package D0225009;
+public class SaldiJhy {
+    public static void main(String[] args) {
+       int a = 20;
+       String b = Integer.toString(a);
+        System.out.print("Dari tipe data integer ke String\t= "+b);
+        double c = 120.5;
+        String d = Double.toString(c);
+        System.out.print("\nDari tipe data double ke String\t\t= "+d+"\n");
+
+  }
+}

cc75b35f0515f380a1c4f3ed3b06ecfa0192e03b
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-09-28 00:14:24 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,17 @@
+package D0225009;
+public class SaldiJhy {
+    public static void main(String[] args) {
+int a = 7;
+        int b = 3;
+        System.out.print("Sebelum ditukar ");
+        System.out.print("\na = "+a);
+        System.out.print("\nb = "+b);
+        a = a + b;
+        b = a-b;
+        a = a-b;
+        System.out.print("\nSesudah ditukar");
+        System.out.print("\na = "+a);
+        System.out.print("\nb = "+b+" \n");
+
+  }
+}

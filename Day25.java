75958378d4a5fc78e67bbb19968d7bb109d3c3ed
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-10-02 00:35:37 +0800

    Update Day25.java

--- Day25.java
+++ Day25.java
@@ -7,8 +7,8 @@ Scanner in = new Scanner(System.in);
         int angka = in.nextInt();
         System.out.println("Masukkan angka kedua : ");
         int angka1 = in.nextInt();
-        angka++;
-        angka1--;
+        angka++;//Increament Menambah value, contoh 5 jika di increament seperti 5++ maka akan menambah value menjadi = 6
+        angka1--;//Decreament Mengurangi value, contoh 10 jika di decreament seperti 10-- maka akan mengurangi value menjadi = 9
         System.out.print("Increment\t: "+angka);
         System.out.print("\nDecreament\t: "+angka1);
         System.out.print("\n");

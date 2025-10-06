1a101c8f98a9f2112ee263692f11366edc4d88d4
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-10-06 22:19:24 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,14 @@
+package Saldi;
+import java.util.Scanner;
+public class Saldi {
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+        System.out.print("a = ");
+        int a = in.nextInt();
+        System.out.print("b = ");
+        int b = in.nextInt();
+        System.out.print(a<b);// "<" untuk membandingkan nilai 1 dengan nilai lainnya dengan nilai a itu lebih kecil dari b
+        System.out.print("\n"+(a>b));// ">" untuk membandingkan nilai 1 dengan nilai lainnya dengan nilai a itu lebih besar dari b
+        System.out.println();
+    }
+}

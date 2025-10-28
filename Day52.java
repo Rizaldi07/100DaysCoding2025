059704ce012b8b1c6785a7804bbfa0adb3e4c0ae
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-10-28 15:59:46 +0800

    Day51.java

---
+++ Day52.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+System.out.print("Masukkan batas : ");
+int batas = in.nextInt();
+for (int i = 1; i <=batas ; i++){
+    System.out.println(i+". Saya pasti sukses");
+}
+	}
+}

5e879403deb344ed170285ffc97396b872d33627
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-10-29 13:48:59 +0800

    Day52.java

---
+++ Day52.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Masukkan awal : ");
+		int awal = in.nextInt();
+		System.out.print("Masukkan batas : ");
+		int batas = in.nextInt();
+		
+		while ( awal <= batas){
+		    
+		    System.out.println(awal+". Aku Pasti Suskseeesssss,,,AMINNNNN");
+		    awal++;
+		}
+	}
+}

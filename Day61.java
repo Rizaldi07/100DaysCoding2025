4c3228c4258c605cfec8143e29106f715afdebf2
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-11-07 22:56:35 +0800

    Day61.java
    Add functionality to print multiples of M up to N.

--- Day61.java
+++ Day61.java
@@ -1,1 +1,23 @@
+package rizaldi1;
+import java.util.Arrays;
+import java.util.Scanner;
 
+
+public class BantaiARRAY {
+    public static void main(String[] args) {
+       Scanner in = new Scanner (System.in);
+        System.out.print("Masukkan angka N : ");
+        int N = in.nextInt();
+        System.out.print("Masukkan angka M : ");
+        int M = in.nextInt();
+        int i = 1;
+        System.out.println("Kelipatan "+M+" dari 1-"+N+" adalah ");
+        do{
+           if(i%M==0){ 
+               System.out.print(i+" ");
+               
+           }i++;
+        }   while(i<=N);
+        
+    }
+}

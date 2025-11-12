164f0547847df811c6cff04773e6054458a57f9b
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-11-12 23:09:57 +0800

    Day66.java

---
+++ Day66.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.print("masukkan batas : ");
+	int a = in.nextInt();
+	int b = 1;
+	do{
+	    System.out.println("\t*");
+	    b++;	
+	}while(a>=b);
+	}
+}

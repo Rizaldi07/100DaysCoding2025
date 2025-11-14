ac97c1d845295a8ab3e9c005e939330bb051a250
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-11-14 22:12:56 +0800

    Day68.java

---
+++ Day68.java
@@ -0,0 +1,21 @@
+package rizaldi1;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class BantaiARRAY {
+    public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan batas : ");
+        int batas = in.nextInt();
+        for (int i = 0; i < batas; i++) {
+            
+            for (int j = 0; j < batas; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+          
+        }
+}

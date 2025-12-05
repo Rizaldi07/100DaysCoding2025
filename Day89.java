3b40c1ddfdbed5a8bb3b3229f71ed723af866e79
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-12-05 14:56:50 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+
+		public static void main(String[] args) {
+		    a();
+		
+	}
+	static void a(){
+	   
+		String kata = "Rizaldi";
+		String kita = kata.substring(0,5);// maka Rizaldi menjadi Rizal
+		System.out.println("Substring : "+kita);
+		String j = "jurusan saya adalah Sisfo";
+		String j1 = j.replace("Sisfo","Informatika");//Mengubah sisfi menjadi informatika
+		System.out.println("replace : "+j1);
+		String u = " usia saya 18 tahun ";
+		String a = u.trim();//menghilangkan spasi awal dan akhir
+		System.out.println("trim : "+a);
+	
+	}
+}

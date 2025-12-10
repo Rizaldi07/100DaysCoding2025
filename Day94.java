fd03455dc39317ee7c0f51990c4d88fda3b354f1
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-12-10 23:17:45 +0800

    Create Day94.java

---
+++ Day94.java
@@ -0,0 +1,10 @@
+
+public class Soal69a {
+    public static void main(String[] args) {
+   aku();
+    }static void aku(){
+        String a = "Rizaldi";
+        String b = "GG";
+            System.out.println(a+" "+b);
+    }	
+}

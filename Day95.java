8f722601ce5804b048098a0e81988f4ae2f4fbc4
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-12-11 22:11:17 +0800

    Create Day95.java

---
+++ Day95.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Soal69a {
+    public static void main(String[] args) {
+nama("Rizaldi","Informatika");
+    }	static void nama(String a,String b){
+      
+            System.out.println("Nama \t: "+a);
+            System.out.println("Prodi \t: "+b);
+    }
+}

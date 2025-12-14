5f3b87d802d12d31ce300e9abbd583dd8110b736
Rizaldi07 <reskyreskyafandy@gmail.com>
2025-12-14 22:46:19 +0800

    Implement Fibonacci series in Day98.java

---
+++ Day98.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner in =new Scanner (System.in);
+		System.out.print("Masukkan batas deret : ");
+		int batas = in.nextInt();
+		int a =0;
+		int b =1;
+		System.out.print("Deret Fibonacci : ");
+		for(int i=0;i<=batas;i++){
+		    System.out.print(a+" ");
+		    int c =a+b;a=b;b=c
+		}
+	}
+}
